b37c309ed0c45c6a41ea1755a3e6f3a66d89058d
m1rqz <ratmirlazarev858@gmail.com>
2026-09-22 22:07:35 +0500

    Загрузил все задания

---
+++ СЕЙФ.py
@@ -0,0 +1,31 @@
+print("=" * 40)
+print("     Cистема безопастности: Взлом")
+print("=" * 40)
+print("Агент,вам нужно пройти 3 уровня защиты")
+print("Будьте осторожны: ошибиться нельзя!")
+low = 1
+high = 100
+max_attempts = 3
+level1_passed = False
+for attempt in range(1, max_attempts + 1):
+    secret_code = (low + high)  //2
+    print("Попытка", attempt, "из", max_attempts)
+    user_input = input("Введите число: ")
+if not user_input.isdigit():
+    print("Ошибка! Нужно ввести целое число.")
+    continue
+guess = int(user_input)
+if guess == secret_code:
+    print("КОД ВЕРНЫЙ!")
+    print("УРОВЕНЬ 1 ПРОЙДЕН!")
+    level1_passed = True
+    break
+elif guess < secret_code:
+    print("Загадоное число больше.")
+    low = secret_code + 1
+else:
+    print("Загадоное число меньше.")
+    high = secret_code - 1
+if not level1_passed:
+    print("ТРЕВОГА! Система заюлокирована. Попытки исчерпаны.")
+    is_active = False
---
+++ Самостоятельная.py
@@ -0,0 +1,29 @@
+"""
+def introduce(name,age):
+    print(f"Привет,я Ратмир,мне 17 лет")
+# introduce("Ратмир",17)
+introduce(age=17 , name="РАтмир")
+"""
+"""
+def suma(a,d):
+     return a+d
+print("Код выполнен")
+suma(2,3)
+result = suma(2,3)
+print("suma", result)
+
+"""
+"""
+def greet(name="Кого выебали"):
+    print(name)
+greet()
+greet("Диму")
+
+"""
+"""
+def square(namber):
+    return namber * namber
+print(square = (5))
+print(square = (-10))
+print(square = (0))
+"""
